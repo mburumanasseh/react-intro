9172926e3f6f0d1c23359117d3ef19550460b48c
Skylar-Lorena <lorayne911@gmail.com>
2026-04-28 10:11:34 +0300

    feat: create reusable Greeting component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Greeting from './Greeting';
 
 function App() {
 const name = "Student";
   return (
     //jsx allows js inside of UI
+    <>
   <h1>Hello {name}</h1>
+  <Greeting/>
+  </>
   )
 }
 
---
+++ src/Greeting.jsx
@@ -0,0 +1,6 @@
+//Greeting component
+function Greeting(){
+    return <h2>Welcome to React</h2>
+}
+
+export default Greeting;
