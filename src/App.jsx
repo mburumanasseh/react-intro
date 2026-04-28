75a236a5a54a41a19cc79b5f5c4d483adae4ee57
Skylar-Lorena <lorayne911@gmail.com>
2026-04-28 10:27:02 +0300

    style: add basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     //jsx allows js inside of UI
     //components can receive data- we call that props
     <>
+    <h1 className='hero'>Welcome to the Student Board</h1>
   <Greeting name ="Eugene"/> 
 </>
   )
